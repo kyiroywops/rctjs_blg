64bdf41312c5abdb4e3d265097193cf2d6b810f7
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-08-31 19:28:47 -0400

    css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Inicio from './componentes/Inicio.js';
 import Blog from './componentes/Blog.js';
 import Acercade from './componentes/Acercade.js';
+import styled from 'styled-components';
+import Header from './componentes/Header.js';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        
-        <main>
+      <ContenedorPrincipal>
+        <Header />
+        <Main>
             <Routes>
               <Route path="/" element={
                 <Inicio />
@@ -24,12 +26,28 @@ const App = () => {
                   <Acercade />
               } />
             </Routes>
-        </main>
+        </Main>
 
         
-      </div>
+      </ContenedorPrincipal>
     </BrowserRouter>
     );
 }
  
+const ContenedorPrincipal = styled.div`
+  padding: 40px;
+  witdh: 90%;
+  max-width: 700px;
+`;
+
+const Main = styled.main`
+  background-color: #fff;
+  padding: 40px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129,129,129,0.1);
+  
+  
+  
+`;
+
 export default App;
--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () => {
     return ( 
-        <header>
+        <ContenedorHeader>
 
-          <h1> Blog </h1>
-          <nav>
+          <Titulo> Blog </Titulo>
+          <Menu>
             <NavLink to="/">Inicio</NavLink>
             <NavLink to="/blog" >Blog</NavLink>
             <NavLink to="acercade">Acerca de</NavLink>
-          </nav>
-        </header>
+          </Menu>
+        </ContenedorHeader>
      );
 }
+
+const ContenedorHeader = styled.header`
+    text-align: center;
+    margin-bottom: 40px;
+    
+`;
  
+const Titulo = styled.h1`
+    margin-bottom: 10px;
+    font-size: 26px;
+    text-transform: uppercase;
+`;
+
+const Menu = styled.nav`
+    a {
+        text-decoration: none;
+        color: #165168;
+        margin: 0 10px;
+    }
+    a:hover {
+        color: #19168
+
+    }
+   
+    a.active {
+        border-bottom: 2px solid #165168;
+        padin-bottom: 3px;
+    }
+`;
+
 export default Header;
