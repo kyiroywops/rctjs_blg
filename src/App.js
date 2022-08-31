bfba0a0362f8d1e65b3b8d3087e054dee29917f4
kyiroywops <102992278+kyiroywops@users.noreply.github.com>
2022-08-31 18:28:05 -0400

    rutas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <div>
+        <header>
 
+          <h1> Blog </h1>
+          <nav>
+            <NavLink to="/">Inicio</NavLink>
+            <NavLink to="/blog" >Blog</NavLink>
+            <NavLink to="acercade">Acerca de</NavLink>
+          </nav>
+          <main>
+            <Routes>
+              <Route path="/" element={
+                <div>
+                  <h2>Pagina de inicio</h2>
+                  <p>Esta es la pagina inicial del sitio.</p>
+                </div>
+
+              } />
+              <Route path="/blog" element={
+                <div>
+                  <h2>Blog</h2>
+                  <ul>
+                    <li>Articulo numero 1</li>  
+                    <li>Articulo numero 2</li>
+                    <li>Articulo numero 3</li>
+                  </ul>
+                </div>
+              } />
+              <Route path="acercade" element={
+                <div>
+                  <h2>Acerca de</h2>
+                  <p>Esta es la pagina de acerca de.</p>
+                </div>
+              } />
+            </Routes>
+          </main>
+
+        </header>
+      </div>
+    </BrowserRouter>
+    );
+}
+ 
 export default App;
